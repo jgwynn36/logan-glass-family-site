c2900f8ca6c5f12cfc583140dabcb0d140ee708e
Jacqueline Gwynn <jacquelinegwynn@jacquelines-air.home>
2020-06-14 23:24:23 -0400

    update source code

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,10 @@
         <li><router-link to="/family-tree">Family Tree</router-link></li>
         <li><router-link to="/events">Events</router-link></li>
     </ul>
+
     <img src="./assets/familyReunionLogo.jpeg">
     <h1>Logan Glass Family Reunion</h1>
-
-
+ 
     <router-view class="view"></router-view>
 
 </div>
@@ -28,4 +28,5 @@ export default {
   .router-link-active{
     color: red ;
   }
+
 </style>

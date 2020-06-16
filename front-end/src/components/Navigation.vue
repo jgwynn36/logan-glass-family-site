62ccccb8571b3c38e580296648994ed23ae1c9bd
Jacqueline Gwynn <jacquelinegwynn@jacquelines-air.home>
2020-06-15 20:48:41 -0400

    update Home and Nav Bar with vuetify template

--- front-end/src/components/Navigation.vue
+++ front-end/src/components/Navigation.vue
@@ -1,43 +1,50 @@
 <template>
-    <div>
-        <h2>Nav Bar</h2>
-        <nav>
-            <router-link class="spacing" v-for="routes in links"
-            v-bind:key="routes.id"
-            :to="`${routes.page}`">{{routes.text}}</router-link>
-        </nav>
-    </div>
+  <div>
+    <v-toolbar
+      dark
+      prominent
+      src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Nav Bar</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>Navigation-export</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'Navigation',
-  data () {
-    return {
-      links: [
-        {
-          id: 0,
-          text: 'Hello World',
-          page: '/HelloWorld'
-        },
-        {
-          id: 1,
-          text: 'Home',
-          page: '/Home'
-        },
-        {
-          id: 2,
-          text: 'About',
-          page: '/About'
-        },
-        {
-          id: 3,
-          text: 'Contact',
-          page: '/Contact'
-        }
-      ]
-    }
-  }
+  data: () => ({
+    links: [
+      {
+        id: 0,
+        text: 'Hello World',
+        page: '/HelloWorld'
+      },
+      {
+        id: 1,
+        text: 'Home',
+        page: '/Home'
+      },
+      {
+        id: 2,
+        text: 'About',
+        page: '/About'
+      },
+      {
+        id: 3,
+        text: 'Contact',
+        page: '/Contact'
+      }
+    ]
+  })
 }
 </script>
 

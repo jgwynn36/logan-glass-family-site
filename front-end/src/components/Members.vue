5ab6320abfb8d60e940fabc9928e845d3ce0118f
Jacqueline Gwynn <jacquelinegwynn@jacquelines-air.home>
2020-06-19 22:39:21 -0400

    add team image and VA members

--- front-end/src/components/Members.vue
+++ front-end/src/components/Members.vue
@@ -1,8 +1,17 @@
 <template>
-    <div>
-        <h1>Reunion Committee Members</h1>
-        <p>Here is some information.  This is the Members page</p>
-    </div>
+<div>
+  <h1>Reunion Committee Members</h1>
+  <img src="../assets/team-img.jpeg" alt="Team">
+  <h2>Virginia Committee Members</h2>
+  <li>Genette Brown-Lanier</li>
+  <li>Robin Cobbs</li>
+  <li>John Glass</li>
+  <li>Kelvin Glass</li>
+  <li>Ashley Gwynn-Keene</li>
+  <li>Jacqueline Gwynn</li>
+  <li>Nancy Gwynn</li>
+  <li>Carolyn Troy</li>
+</div>
 </template>
 
 <script>
@@ -10,6 +19,7 @@ export default {
   name: 'Members' // this is the name of the compnent
 }
 </script>
+
 <style>
 
 </style>
